963fcdebfed48d2d76b5bf422ad23f726c57ce5b
Adrian Reyes <adrian_reyes1@yahoo.com>
2021-12-08 23:49:47 -0800

    got the character to start moving

---
+++ script.js
@@ -0,0 +1,30 @@
+//function to get character to move
+function moveTron() {
+    let tron = document.querySelector('#character');
+    let moveBy = 20;
+
+    window.addEventListener('load', () => {
+        tron.style.position = 'relative';
+        tron.style.left = 0;
+        tron.style.top = 0;
+    });
+    window.addEventListener('keydown', (e) => {
+        switch (e.key) {
+            case 'ArrowLeft':
+                tron.style.left = parseInt(tron.style.left) - moveBy + 'px';
+                break;
+            case 'ArrowRight':
+                tron.style.left = parseInt(tron.style.left) + moveBy + 'px';
+                break;
+            case 'ArrowUp':
+                tron.style.top = parseInt(tron.style.top) - moveBy + 'px';
+                break;
+            case 'ArrowDown':
+                tron.style.top = parseInt(tron.style.top) + moveBy + 'px';
+                break;
+        }
+    });
+}
+moveTron()
+
+//
