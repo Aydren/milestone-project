41ae8cf56ae36835e9bb51a2ebe94cc22a3982d4
Adrian Reyes <adrian_reyes1@yahoo.com>
2021-12-12 10:40:46 -0800

    This is a update/reset of my project with new css from another game I have made

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-//function to get character to move
+
+/* //function to get character to move
 function moveTron() {
     let tron = document.querySelector('#character');
     let moveBy = 20;
 
+// loads the location of Tron
     window.addEventListener('load', () => {
         tron.style.position = 'relative';
         tron.style.left = 0;
         tron.style.top = 0;
     });
+
+//moves the character in the direction you want when pressed down
     window.addEventListener('keydown', (e) => {
         switch (e.key) {
-            case 'ArrowLeft':
+            case 'ArrowLeft': //move left
                 tron.style.left = parseInt(tron.style.left) - moveBy + 'px';
                 break;
-            case 'ArrowRight':
+            case 'ArrowRight': //move right
                 tron.style.left = parseInt(tron.style.left) + moveBy + 'px';
                 break;
-            case 'ArrowUp':
+            case 'ArrowUp': //move up
                 tron.style.top = parseInt(tron.style.top) - moveBy + 'px';
                 break;
-            case 'ArrowDown':
+            case 'ArrowDown': //move down
                 tron.style.top = parseInt(tron.style.top) + moveBy + 'px';
                 break;
         }
     });
 }
-moveTron()
-
-//
+moveTron() */
